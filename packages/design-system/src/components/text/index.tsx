b6353446950bc28ce4285f277ab56a2e3bf69096
Ash <ashley@juice49.com>
2020-01-26 00:07:55 +0000

    Style experimentation

--- packages/design-system/src/components/text/index.tsx
+++ packages/design-system/src/components/text/index.tsx
@@ -1,14 +1,27 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from 'react'
 
 interface Props {
   /**
    * The text.
    */
-  children: React.ReactNode
+  children: React.ReactNode,
+  className?: string,
+  sx?: any
 }
 
-const Text: React.FC<Props> = ({ children }) => (
-  <span>{children}</span>
+const Text: React.FC<Props> = ({ children, className, sx }) => (
+  <span
+    className={className}
+    sx={{
+      ...sx,
+      display: 'inline-block',
+      bg: 'yellow'
+    }}
+  >
+    {children}
+  </span>
 )
 
 export default Text
--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,8 +1,19 @@
+/** @jsx jsx */
 import React from 'react'
+import { jsx } from 'theme-ui'
 import Text from '@cssinjs/design-system/src/components/text'
 
 const Page: React.FC = () => (
-  <Text>Welcome</Text>
+  <Text
+    sx={{
+      color: 'primary',
+      padding: '100px',
+      // backgroundColor: 'red'
+    }}
+    // as='h1'
+  >
+    Welcome
+  </Text>
 )
 
 export default Page
